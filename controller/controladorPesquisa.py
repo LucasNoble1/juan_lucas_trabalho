69c252ca22d8899803e834c542cfd30b8c5378e1
LucasNoble1 <56458675+LucasNoble1@users.noreply.github.com>
2022-12-16 16:39:29 -0300

    Update controladorPesquisa.py

--- controller/controladorPesquisa.py
+++ controller/controladorPesquisa.py
@@ -1,11 +1,17 @@
 from model.pesquisa import Pesquisa
 #from DAOs.cursoDAO import CursoDAO
+from view.telaPesquisa import TelaPesquisa
 
 
 class ControladorPesquisa():
     def __init__(self):
         #self.__curso_dao = CursoDAO()
         self.__pesquisa = Pesquisa()
+        self.__tela = TelaPesquisa(self)
+
+    def tela_pesquisa(self):
+        self.__tela.tela_opcoes()
+
 
     def add_cpf_curso(self, curso, cpf):
         if curso in self.__pesquisa.curso:
@@ -31,39 +37,53 @@ class ControladorPesquisa():
             self.__pesquisa.curso[curso] = lista
 
     def verifica_idade(self,idade):
-        if idade < 18:
-            return "<18"
-        if idade >= 18 and idade < 30:
-            return "18a29"
-        if idade >= 30 and idade < 40:
-            return "30a39"
-        if idade >= 40 and idade < 50:
-            return "40a49"
-        if idade >= 50:
-            return ">50"
+        if type(idade) == int:
+            if idade < 18:
+                return "<18"
+            if idade >= 18 and idade < 30:
+                return "18a29"
+            if idade >= 30 and idade < 40:
+                return "30a39"
+            if idade >= 40 and idade < 50:
+                return "40a49"
+            if idade >= 50:
+                return ">=50"
+        else:
+            return idade
 
     def add_cpf_idade(self, idade, cpf):
         chave_idade = self.verifica_idade(idade)
-        lista = self.__pesquisa.curso[chave_idade]
+        print(chave_idade)
+        lista = self.__pesquisa.idade[chave_idade]
         lista.append(cpf)
         self.__pesquisa.idade[chave_idade] = lista
         print(self.__pesquisa.idade)
 
     def alterar_cpf_idade(self, idade, nova_idade, cpf):
-        pass
-    def excluir_cpf_idade(self, curso, cpf):
-        pass
+        self.excluir_cpf_idade(idade, cpf)
+        self.add_cpf_idade(nova_idade, cpf)
+
+    def excluir_cpf_idade(self, idade, cpf):
+        chave_idade = self.verifica_idade(idade)
+        lista = self.__pesquisa.idade[chave_idade]
+        lista.remove(cpf)
+        self.__pesquisa.idade[chave_idade] = lista
 
 
-    def add_cpf_ano_ingresso(self, idade, cpf):
-        pass
-    def alterar_cpf_ano_ingresso(self, idade, nova_idade, cpf):
-        pass
-    def excluir_cpf_ano_ingresso(self, curso, cpf):
-        pass
+    def add_cpf_ano_ingresso(self, ano_ingresso, cpf):
+        if ano_ingresso in self.__pesquisa.ano_ingresso:
+            lista = self.__pesquisa.ano_ingresso[ano_ingresso]
+            lista.append(cpf)
+            self.__pesquisa.ano_ingresso[ano_ingresso] = lista
+            print(self.__pesquisa.ano_ingresso)
+        else:
+            self.__pesquisa.ano_ingresso[ano_ingresso] = [cpf]
+            print(self.__pesquisa.ano_ingresso)
+    def alterar_cpf_ano_ingresso(self, ano_ingresso, novo_ano_ingresso, cpf):
+        self.excluir_cpf_ano_ingresso(ano_ingresso, cpf)
+        self.add_cpf_ano_ingresso(novo_ano_ingresso, cpf)
 
-
-
-
-
-
+    def excluir_cpf_ano_ingresso(self, ano_ingresso, cpf):
+        lista = self.__pesquisa.ano_ingresso[ano_ingresso]
+        lista.remove(cpf)
+        self.__pesquisa.ano_ingresso[ano_ingresso] = lista
